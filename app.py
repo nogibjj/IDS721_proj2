0a0570df14a6fd63182b98786378c4fa2b245014
Yuxin Song <ys385@duke.edu>
2023-02-14 23:34:52 -0500

    fix bug

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def get_city_state(zipcode):
-    return "Welcome to the Zipcode API!"
-
 @app.route('/<zipcode>')
 def get_city_state(zipcode):
     url = 'https://api.zippopotam.us/us/'+str(zipcode)
@@ -16,6 +12,10 @@ def get_city_state(zipcode):
     state = data['places'][0]['state abbreviation']
     city_state = f"{city}, {state}"
     return jsonify({'city_state': city_state})
+
+# @app.route('/')
+# def get_index(zipcode):
+#     return "Welcome to the Zipcode API!"
 
 if __name__ == '__main__':
     app.run()
